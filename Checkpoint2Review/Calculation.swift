4c1a289bed118ff7e5fa4e41913a4c0772cd6a55
gholyra <tiago.mlyra@gmail.com>
2024-05-08 20:19:16 -0300

    Initial Commit

---
+++ Checkpoint2Review/Calculation.swift
@@ -0,0 +1,16 @@
+//
+//  Calculation.swift
+//  Checkpoint2Review
+//
+//  Created by Usuário Convidado on 08/05/24.
+//
+
+import Cocoa
+
+class Calculation: NSObject {
+
+    override init() {
+        
+    }
+    
+}
