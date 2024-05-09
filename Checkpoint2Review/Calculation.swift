62e841b2585079a5071d2b392345837c07b5f0f9
gholyra <tiago.mlyra@gmail.com>
2024-05-08 21:02:08 -0300

    Acompanhamento - Aula 08/05

--- Checkpoint2Review/Calculation.swift
+++ Checkpoint2Review/Calculation.swift
@@ -5,12 +5,20 @@
 //  Created by Usuário Convidado on 08/05/24.
 //
 
-import Cocoa
+import UIKit
 
-class Calculation: NSObject {
+class Calculation {
 
-    override init() {
-        
+    var n1:Float!
+    var n2:Float!
+    
+    init() {
+        self.n1 = 0
+        self.n2 = 0
+    }
+    
+    func sum()-> Float {
+        return n1 + n2
     }
     
 }
--- Checkpoint2Review/ViewController.swift
+++ Checkpoint2Review/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtFieldN1: UITextField!
+    @IBOutlet weak var txtFieldN2: UITextField!
+    @IBOutlet weak var txtFieldResult: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func btnCalculate(_ sender: Any) {
+        let calculationObj = Calculation()
+        
+        calculationObj.n1 = Float(txtFieldN1.text!)
+        calculationObj.n2 = Float(txtFieldN2.text!)
+        
+        txtFieldResult.text = String(calculationObj.sum())
+    }
+    
 
 }
 
